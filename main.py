53f93d4d5d560eec5b447527258bdb395e95c632
alceil <ashishshajisrampickal@gmail.com>
2020-12-07 17:01:15 +0530

    added notifu feature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from plyer import notification;
+import requests;
+from bs4 import BeautifulSoup
 
 def notifyMe(title,message):
     notification.notify(
@@ -6,6 +8,26 @@ def notifyMe(title,message):
         message =message,
         timeout = 6
     )
+def getData(url):
+    r = requests.get(url)
+    return r.text    
 
 if __name__ == "__main__":
     notifyMe("Kindi","Odra myre")    
+    myhtmldata = getData("https://www.mohfw.gov.in/")
+    print(myhtmldata)
+    soup = BeautifulSoup(myhtmldata,'html.parser')
+    print(soup.prettify)
+    myDatastr = ""
+    for tr in soup.find_all('tbody')[0].find_all('tr'):
+        myDatastr += tr.get_text() 
+    itemList = myDatastr.split("\n\n")
+    states = ['','']
+    for item in itemList[0:22]:
+        datalist = item.split("\n\n")
+        if datalist[1]  in states:
+            
+
+
+
+
